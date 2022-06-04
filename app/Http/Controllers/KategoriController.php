6ce24e7dc8e4ec2d8fc082295f56baced10cc360
= <=>
2022-06-04 20:41:57 +0700

    Controller

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Kategori;
 
 use Illuminate\Http\Request;
 
@@ -8,5 +9,28 @@ class KategoriController extends Controller
 {
     public function kategori(){
         return view('kategoris.kategori');
+    }
+    public function tambahkategori(){
+        return view('kategoris.tambahkategori');
+    }
+    public function tampilKategori($id){
+        $data = Kategori::find($id);
+        return view('kategoris.tampilKategori', compact('data'));
+    }
+    public function insertKategori(Request $request){
+        Kategori::create($request->all());
+
+        return redirect()->route('kategori')->with('success', 'Data Kategori Berhasil Di Tambahkan');
+    }
+    public function updateKategori(Request $request, $id){
+        $data = Kategori::find($id);
+        $data ->update($request->all());
+
+        return redirect()->route('kategori')->with('success', 'Data Kategori Berhasil Di Update');
+    }
+    public function delete($id){
+        $data = Kategori::find($id);
+        $data->delete();
+        return redirect()->route('kategori')->with('success', 'Data Kategori Berhasil Di Hapus');
     }
 }
