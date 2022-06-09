bac6fa13485282ac9cddedd5a04a92ab8af7acae
sindyalvinaa <sindyalvinaa93@gmail.com>
2022-06-09 09:17:51 +0700

    Detail

---
+++ resources/views/pengacaras/detail.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Detail Pengacara</h1>
+
+        </div>
+    </section>
+@endsection
