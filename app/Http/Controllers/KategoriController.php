df9b6867ba20d50a32f9cff9d9e94ca4767cee94
= <=>
2022-06-22 20:38:15 +0700

    update controller

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -4,34 +4,42 @@ namespace App\Http\Controllers;
 use App\Models\Kategori;
 use App\Models\Pengacara;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class KategoriController extends Controller
 {
-    public function kategori(){
-        $data = Kategori::paginate(5);
-        return view('kategoris.kategori',compact('data'));
+    public function index(Request $request){
+
+
+        $kategoris = DB::table('kategoris')
+        ->when($request->input('kategori'), function ($query, $kategori) {
+            return $query->where('kategori', 'like', '%' . $kategori . '%');
+        })
+        ->select('id', 'kategori', 'keterangan')
+        ->paginate(10);
+        return view('user.kategori', compact('kategoris'));
     }
     public function tambahKategori(){
         return view('kategoris.tambahKategori');
     }
     public function tampilKategori($id){
-        $data = Kategori::find($id);
-        return view('kategoris.tampilKategori', compact('data'));
+        $kategori = Kategori::find($id);
+        return view('kategoris.tampilKategori', compact('kategori'));
     }
     public function insertKategori(Request $request){
         Kategori::create($request->all());
 
-        return redirect()->route('kategori')->with('success', 'Data Kategori Berhasil Di Tambahkan');
+        return redirect()->route('kategori')->with('success', 'kate$kategori Kategori Berhasil Di Tambahkan');
     }
     public function updateKategori(Request $request, $id){
-        $data = Kategori::find($id);
-        $data ->update($request->all());
+        $kategori = Kategori::find($id);
+        $kategori ->update($request->all());
 
-        return redirect()->route('kategori')->with('success', 'Data Kategori Berhasil Di Update');
+        return redirect()->route('kategori')->with('success', 'kate$kategori Kategori Berhasil Di Update');
     }
     public function delete($id){
-        $data = Kategori::find($id);
-        $data->delete();
-        return redirect()->route('kategori')->with('success', 'Data Kategori Berhasil Di Hapus');
+        $kategori = Kategori::find($id);
+        $kategori->delete();
+        return redirect()->route('kategori')->with('success', 'kate$kategori Kategori Berhasil Di Hapus');
     }
 }
