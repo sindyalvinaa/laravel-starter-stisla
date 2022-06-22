7b334322b09c5cf425bb1344e7479c701e5627ae
= <=>
2022-06-22 21:38:35 +0700

    updateController

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -10,36 +10,31 @@ class KategoriController extends Controller
 {
     public function index(Request $request){
 
-
-        $kategoris = DB::table('kategoris')
-        ->when($request->input('kategori'), function ($query, $kategori) {
-            return $query->where('kategori', 'like', '%' . $kategori . '%');
-        })
-        ->select('id', 'kategori', 'keterangan')
-        ->paginate(10);
-        return view('user.kategori', compact('kategoris'));
+        $data = Kategori::paginate(5);
+        return view('kategoris.kategori',compact('data'));
     }
     public function tambahKategori(){
         return view('kategoris.tambahKategori');
     }
     public function tampilKategori($id){
-        $kategori = Kategori::find($id);
-        return view('kategoris.tampilKategori', compact('kategori'));
+        $data = Kategori::find($id);
+        return view('kategoris.tampilKategori', compact('data'));
     }
     public function insertKategori(Request $request){
         Kategori::create($request->all());
 
         return redirect()->route('kategori')->with('success', 'kate$kategori Kategori Berhasil Di Tambahkan');
+
     }
     public function updateKategori(Request $request, $id){
-        $kategori = Kategori::find($id);
-        $kategori ->update($request->all());
+        $data = Kategori::find($id);
+        $data ->update($request->all());
 
         return redirect()->route('kategori')->with('success', 'kate$kategori Kategori Berhasil Di Update');
     }
     public function delete($id){
-        $kategori = Kategori::find($id);
-        $kategori->delete();
+        $data = Kategori::find($id);
+        $data->delete();
         return redirect()->route('kategori')->with('success', 'kate$kategori Kategori Berhasil Di Hapus');
     }
 }
