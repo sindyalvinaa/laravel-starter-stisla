d221f372de9cda83c727d64f01ba1a3ddfdff326
sindyalvinaa <sindyalvinaa93@gmail.com>
2022-06-27 18:50:31 +0700

    migration

---
+++ database/migrations/2022_06_27_110701_create_kontraks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKontraksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kontraks', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('jenis');
+            $table->string('tanggal');
+            $table->string('isi');
+            $table->string('biaya');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kontraks');
+    }
+}
