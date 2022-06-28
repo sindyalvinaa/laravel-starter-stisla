4ad89d201783c257f2ab32cc873238d6b588d944
RinaHariyanti <rinayanti1771@gmail.com>
2022-06-28 18:43:54 +0700

    update detail

--- resources/views/pengacaras/detail.blade.php
+++ resources/views/pengacaras/detail.blade.php
@@ -5,12 +5,13 @@
             <h1>Detail Pengacara</h1>
         </div>
             <div class="section-body">
-            <div class="card-body">
+            <div class="text-center card-body">
                 <img src="{{ asset('assets/img/'.$pengacara->foto) }}" alt="" style="width:250px; height: 300px" class="mb-3 mt-3">
                 <h5 class="card-title">Name : {{$pengacara->nama_pengacara}}</h5>
                 <p class="card-text">Kategori : {{$pengacara->kategori}}</p>
                 <p class="card-text">Profil : {{$pengacara->profil}}</p>
-                <a href="https://wa.me/6285731177676" class="btn btn-sm btn-info btn-icon"><i class="fas fa-phone"></i> Hubungi Pengacara</a>
+                <p class="card-text">Keterangan : {{$pengacara->keterangan}}</p>
+                <a href="https://wa.me/{{ $pengacara->telp }}" class="btn btn-sm btn-info btn-icon"><i class="fas fa-phone"></i> Hubungi Pengacara</a>
             </div>
             </div>
         </div>
