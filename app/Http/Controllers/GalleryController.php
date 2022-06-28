1bec541084628d37cb011f466b475e3c726bda0f
= <=>
2022-06-28 23:01:31 +0700

    Update Controller

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -5,11 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class GalleryPidanaController extends Controller
+class GalleryController extends Controller
 {
     //
-
-
     public function index(Request $request){
 
 
@@ -21,6 +19,19 @@ class GalleryPidanaController extends Controller
         ->select('id', 'foto','nama_pengacara','kategori', 'profil')
         ->paginate(10);
         return view('user.galleryPidana', compact('pidana'));
+
+    }
+    public function perdata(Request $request){
+
+
+        $perdata = DB::table('pengacaras')
+        ->when($request->input('nama_pengacara'), function ($query, $perdata) {
+            return $query->where('nama_pengacara', 'like', '%' . $perdata . '%');
+        })
+        ->where('kategori','Hukum Perdata')
+        ->select('id', 'foto','nama_pengacara','kategori', 'profil')
+        ->paginate(10);
+        return view('user.galleryPerdata', compact('perdata'));
 
     }
 }
