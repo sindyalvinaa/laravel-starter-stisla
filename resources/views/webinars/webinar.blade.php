e75b40b270fa5f263e15274e4805c63674fffcb0
RinaHariyanti <rinayanti1771@gmail.com>
2022-06-28 18:12:26 +0700

    webinar.blade

---
+++ resources/views/webinars/webinar.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Webinar</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                <div class="breadcrumb-item"><a href="#">Components</a></div>
+                <div class="breadcrumb-item">Table</div>
+            </div>
+        </div>
+        <div class="bg-light p-5 rounded">
+            <img src="../assets/img/webinar.png" alt="" style="width:1020px; height: 600px" align="center" class="mb-5 mt-5 ">
+        </div>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSeVLbrFFiXkgtuKY_d2IKsj1KeDhcVPCSq-elE-wMvMNMB86A/viewform"><h5 class="text-center center-btn btn-center btn-info">DAFTAR SEBAGAI SUPORTER</h5></a>
+        </div>
+    </section>
+@endsection
